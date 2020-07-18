536b5f20e9d6fad22c5363a7787be47c8ba1df12
Jaha Ereshov <jaha@ereshov.ru>
2020-07-18 08:21:23 +0400

    add doc comments

--- validators.go
+++ validators.go
@@ -2,26 +2,35 @@ package validator_kodix
 
 import "regexp"
 
+// DefaultLen - constant to setting validity
+// length of slices, arrays, strings and maps
 const DefaultLen = 7
 
+// Validator interface to check validity of data
 type Validator interface {
 	IsValid() bool
 }
 
+// EmptyChecker interface to check that is variable empty
 type EmptyChecker interface {
 	IsEmpty() bool
 }
 
+// RegularChecker interface to check that string is compliant to pattern
 type RegularChecker interface {
 	Compliant(string) bool
 }
 
+// KodixString wrapper for string data type, with interfaces implementation possibility
 type KodixString string
 
+// KodixIntMap wrapper for [int: int] map data type, with interfaces implementation possibility
 type KodixIntMap map[int]int
 
+// KodixSlice wrapper for slice data type, with interfaces implementation possibility
 type KodixSlice []int
 
+// KodixArray wrapper for  array of length 7 data type, with interfaces implementation possibility
 type KodixArray [7]int
 
 func (ks KodixString) Compliant(pattern string) bool {
@@ -71,6 +80,7 @@ func validLen() int {
 	return DefaultLen
 }
 
+// InIntArray - function to define that exist number in number slice or not
 func InIntArray(v int, arrV []int) bool {
 	for _, i := range arrV {
 		if v == i {
@@ -80,6 +90,7 @@ func InIntArray(v int, arrV []int) bool {
 	return false
 }
 
+// InStringArray - function to define that exist string in string slice or not
 func InStringArray(v string, arrV []string) bool {
 	for _, i := range arrV {
 		if v == i {
